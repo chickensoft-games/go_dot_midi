2ce45385ac8b6dee4d5ef418e083c86f66eb1b60
Joanna May <joannamadisonmay@gmail.com>
2022-07-08 15:34:32 -0500

    Fix warnings

--- src/csharpsynth/AudioSynthesis/Bank/Components/Effects/Delay.cs
+++ src/csharpsynth/AudioSynthesis/Bank/Components/Effects/Delay.cs
@@ -2,37 +2,39 @@
   using System;
 
   public class Delay : IAudioEffect {
-    private float[] buffer1;
-    private float[] buffer2;
-    private int position1;
-    private int position2;
+    private readonly float[] _buffer1;
+    private readonly float[] _buffer2;
+    private int _position1;
+    private int _position2;
 
     public Delay(int sampleRate, double delay) {
-      buffer1 = new float[(int)(sampleRate * delay)];
-      position1 = 0;
+      _buffer1 = new float[(int)(sampleRate * delay)];
+      _position1 = 0;
+      _buffer2 = null!;
+      _position2 = 0;
     }
     public Delay(int sampleRate, double delay1, double delay2) {
-      buffer1 = new float[(int)(sampleRate * delay1)];
-      position1 = 0;
-      buffer2 = new float[(int)(sampleRate * delay2)];
-      position2 = 0;
+      _buffer1 = new float[(int)(sampleRate * delay1)];
+      _position1 = 0;
+      _buffer2 = new float[(int)(sampleRate * delay2)];
+      _position2 = 0;
     }
     public void ApplyEffect(float[] source) {
-      int x = 0, end = buffer1.Length - 1;
+      int x = 0, end = _buffer1.Length - 1;
       while (x < source.Length) {
         if (source.Length - x >= end) {
-          while (position1 < end) {
-            buffer1[position1++] = source[x];
-            source[x++] = buffer1[position1];
+          while (_position1 < end) {
+            _buffer1[_position1++] = source[x];
+            source[x++] = _buffer1[_position1];
           }
-          buffer1[position1] = source[x];
-          position1 = 0;
-          source[x++] = buffer1[position1];
+          _buffer1[_position1] = source[x];
+          _position1 = 0;
+          source[x++] = _buffer1[_position1];
         }
         else {
           while (x < source.Length) {
-            buffer1[position1++] = source[x];
-            source[x++] = buffer1[position1];
+            _buffer1[_position1++] = source[x];
+            source[x++] = _buffer1[_position1];
           }
         }
       }
@@ -41,51 +43,52 @@
       int x, end;
       //source1
       x = 0;
-      end = buffer1.Length - 1;
+      end = _buffer1.Length - 1;
       while (x < source1.Length) {
         if (source1.Length - x >= end) {
-          while (position1 < end) {
-            buffer1[position1++] = source1[x];
-            source1[x++] = buffer1[position1];
+          while (_position1 < end) {
+            _buffer1[_position1++] = source1[x];
+            source1[x++] = _buffer1[_position1];
           }
-          buffer1[position1] = source1[x];
-          position1 = 0;
-          source1[x++] = buffer1[position1];
+          _buffer1[_position1] = source1[x];
+          _position1 = 0;
+          source1[x++] = _buffer1[_position1];
         }
         else {
           while (x < source1.Length) {
-            buffer1[position1++] = source1[x];
-            source1[x++] = buffer1[position1];
+            _buffer1[_position1++] = source1[x];
+            source1[x++] = _buffer1[_position1];
           }
         }
       }
       //source2
       x = 0;
-      end = buffer2.Length - 1;
+      end = _buffer2.Length - 1;
       while (x < source2.Length) {
         if (source2.Length - x >= end) {
-          while (position2 < end) {
-            buffer2[position2++] = source2[x];
-            source2[x++] = buffer2[position2];
+          while (_position2 < end) {
+            _buffer2[_position2++] = source2[x];
+            source2[x++] = _buffer2[_position2];
           }
-          buffer2[position2] = source2[x];
-          position2 = 0;
-          source2[x++] = buffer2[position2];
+          _buffer2[_position2] = source2[x];
+          _position2 = 0;
+          source2[x++] = _buffer2[_position2];
         }
         else {
           while (x < source2.Length) {
-            buffer2[position2++] = source2[x];
-            source2[x++] = buffer2[position2];
+            _buffer2[_position2++] = source2[x];
+            source2[x++] = _buffer2[_position2];
           }
         }
       }
     }
     public void Reset() {
-      position1 = 0;
-      position2 = 0;
-      Array.Clear(buffer1, 0, buffer1.Length);
-      if (buffer2 != null)
-        Array.Clear(buffer2, 0, buffer2.Length);
+      _position1 = 0;
+      _position2 = 0;
+      Array.Clear(_buffer1, 0, _buffer1.Length);
+      if (_buffer2 != null) {
+        Array.Clear(_buffer2, 0, _buffer2.Length);
+      }
     }
   }
 }
--- src/csharpsynth/AudioSynthesis/Bank/Components/Effects/Flanger.cs
+++ src/csharpsynth/AudioSynthesis/Bank/Components/Effects/Flanger.cs
@@ -3,14 +3,14 @@
   using AudioSynthesis.Bank.Descriptors;
 
   public class Flanger : IAudioEffect {
-    private readonly int baseDelay;
-    private readonly int minDelay;
-    private readonly float[] inputBuffer1;
-    private readonly float[] outputBuffer1;
-    private int position1;
-    private readonly float[] inputBuffer2;
-    private readonly float[] outputBuffer2;
-    private int position2;
+    private readonly int _baseDelay;
+    private readonly int _minDelay;
+    private readonly float[] _inputBuffer1;
+    private readonly float[] _outputBuffer1;
+    private int _position1;
+    private readonly float[] _inputBuffer2;
+    private readonly float[] _outputBuffer2;
+    private int _position2;
 
     public Lfo Lfo { get; set; }
     public float FeedBack { get; set; }
@@ -25,17 +25,17 @@
       Lfo = new Lfo();
       Lfo.QuickSetup(sampleRate, description);
 
-      baseDelay = (int)(sampleRate * (maxDelay - minDelay));
-      this.minDelay = (int)(sampleRate * minDelay);
+      _baseDelay = (int)(sampleRate * (maxDelay - minDelay));
+      _minDelay = (int)(sampleRate * minDelay);
 
       var size = (int)(sampleRate * maxDelay) + 1;
-      inputBuffer1 = new float[size];
-      outputBuffer1 = new float[size];
-      position1 = 0;
+      _inputBuffer1 = new float[size];
+      _outputBuffer1 = new float[size];
+      _position1 = 0;
 
-      inputBuffer2 = new float[size];
-      outputBuffer2 = new float[size];
-      position2 = 0;
+      _inputBuffer2 = new float[size];
+      _outputBuffer2 = new float[size];
+      _position2 = 0;
 
       FeedBack = .15f;
       WetMix = .5f;
@@ -44,18 +44,18 @@
     public void ApplyEffect(float[] source) {
       for (var x = 0; x < source.Length; x++) {
         Lfo.Increment(1);
-        var index = position1 - (int)((baseDelay * ((.5 * Lfo.Value) + .5)) + minDelay);
+        var index = _position1 - (int)((_baseDelay * ((.5 * Lfo.Value) + .5)) + _minDelay);
 
         if (index < 0) {
-          index += inputBuffer1.Length;
+          index += _inputBuffer1.Length;
         }
 
-        inputBuffer1[position1] = source[x];
-        outputBuffer1[position1] = (DryMix * inputBuffer1[position1]) + (WetMix * inputBuffer1[index]) + (FeedBack * outputBuffer1[index]);
-        source[x] = outputBuffer1[position1++];
+        _inputBuffer1[_position1] = source[x];
+        _outputBuffer1[_position1] = (DryMix * _inputBuffer1[_position1]) + (WetMix * _inputBuffer1[index]) + (FeedBack * _outputBuffer1[index]);
+        source[x] = _outputBuffer1[_position1++];
 
-        if (position1 == inputBuffer1.Length) {
-          position1 = 0;
+        if (_position1 == _inputBuffer1.Length) {
+          _position1 = 0;
         }
       }
     }
@@ -64,39 +64,39 @@
         Lfo.Increment(1);
         var lfoValue = (.5 * Lfo.Value) + .5;
         //source 1
-        index = position1 - (int)((baseDelay * lfoValue) + minDelay);
+        index = _position1 - (int)((_baseDelay * lfoValue) + _minDelay);
         if (index < 0) {
-          index += inputBuffer1.Length;
+          index += _inputBuffer1.Length;
         }
 
-        inputBuffer1[position1] = source1[x];
-        outputBuffer1[position1] = (DryMix * inputBuffer1[position1]) + (WetMix * inputBuffer1[index]) + (FeedBack * outputBuffer1[index]);
-        source1[x] = outputBuffer1[position1++];
-        if (position1 == inputBuffer1.Length) {
-          position1 = 0;
+        _inputBuffer1[_position1] = source1[x];
+        _outputBuffer1[_position1] = (DryMix * _inputBuffer1[_position1]) + (WetMix * _inputBuffer1[index]) + (FeedBack * _outputBuffer1[index]);
+        source1[x] = _outputBuffer1[_position1++];
+        if (_position1 == _inputBuffer1.Length) {
+          _position1 = 0;
         }
         //source 2
-        index = position2 - (int)((baseDelay * (1.0 - lfoValue)) + minDelay);
+        index = _position2 - (int)((_baseDelay * (1.0 - lfoValue)) + _minDelay);
         if (index < 0) {
-          index += inputBuffer2.Length;
+          index += _inputBuffer2.Length;
         }
 
-        inputBuffer2[position2] = source2[x];
-        outputBuffer2[position2] = (DryMix * inputBuffer2[position2]) + (WetMix * inputBuffer2[index]) + (FeedBack * outputBuffer2[index]);
-        source2[x] = outputBuffer2[position2++];
-        if (position2 == inputBuffer2.Length) {
-          position2 = 0;
+        _inputBuffer2[_position2] = source2[x];
+        _outputBuffer2[_position2] = (DryMix * _inputBuffer2[_position2]) + (WetMix * _inputBuffer2[index]) + (FeedBack * _outputBuffer2[index]);
+        source2[x] = _outputBuffer2[_position2++];
+        if (_position2 == _inputBuffer2.Length) {
+          _position2 = 0;
         }
       }
     }
     public void Reset() {
       Lfo.Reset();
-      Array.Clear(inputBuffer1, 0, inputBuffer1.Length);
-      Array.Clear(outputBuffer1, 0, outputBuffer1.Length);
-      Array.Clear(inputBuffer2, 0, inputBuffer2.Length);
-      Array.Clear(outputBuffer2, 0, outputBuffer2.Length);
-      position1 = 0;
-      position2 = 0;
+      Array.Clear(_inputBuffer1, 0, _inputBuffer1.Length);
+      Array.Clear(_outputBuffer1, 0, _outputBuffer1.Length);
+      Array.Clear(_inputBuffer2, 0, _inputBuffer2.Length);
+      Array.Clear(_outputBuffer2, 0, _outputBuffer2.Length);
+      _position1 = 0;
+      _position2 = 0;
     }
   }
 }
