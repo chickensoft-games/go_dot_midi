b001c958c0c54a05a4788b2c577e73710aee0e91
Joanna May <joannamadisonmay@gmail.com>
2022-07-08 12:06:10 -0500

    Format more files

--- src/csharpsynth/AudioSynthesis/Bank/Components/Effects/Chorus.cs
+++ src/csharpsynth/AudioSynthesis/Bank/Components/Effects/Chorus.cs
@@ -1,14 +1,11 @@
-﻿namespace AudioSynthesis.Bank.Components.Effects
-{
-    /// <summary>
-    ///  A simple single layer chorus that works for both mono and stereo input.
-    /// </summary>
-    public class Chorus : Flanger
-    {
-        public Chorus(int sampleRate, double minDelay, double maxDelay)
-            :base(sampleRate,minDelay,maxDelay)
-        {
+﻿namespace AudioSynthesis.Bank.Components.Effects {
+  /// <summary>
+  ///  A simple single layer chorus that works for both mono and stereo input.
+  /// </summary>
+  public class Chorus : Flanger {
+    public Chorus(int sampleRate, double minDelay, double maxDelay)
+        : base(sampleRate, minDelay, maxDelay) {
 
-        }
     }
+  }
 }
--- src/csharpsynth/AudioSynthesis/Bank/Components/Effects/IAudioEffect.cs
+++ src/csharpsynth/AudioSynthesis/Bank/Components/Effects/IAudioEffect.cs
@@ -1,12 +1,10 @@
-﻿namespace AudioSynthesis.Bank.Components.Effects
-{
-    /// <summary>
-    /// Describes an effect that can be applied on a sample buffer.
-    /// </summary>
-    public interface IAudioEffect
-    {
-        void ApplyEffect(float[] source);
-        void ApplyEffect(float[] source1, float[] source2);
-        void Reset();
-    }
+﻿namespace AudioSynthesis.Bank.Components.Effects {
+  /// <summary>
+  /// Describes an effect that can be applied on a sample buffer.
+  /// </summary>
+  public interface IAudioEffect {
+    void ApplyEffect(float[] source);
+    void ApplyEffect(float[] source1, float[] source2);
+    void Reset();
+  }
 }
